ed97543eb02c6130c0343ee3dce4800d1ec031d1
dfsfdsfdsfsd <59228985+dfsfdsfdsfsd@users.noreply.github.com>
2020-01-23 19:04:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $GLOBALS['_ta_debug_mode'] = false; //To enable debug mode, set to true or load 
 
 require 'bootloader.php';
 
-$campaign_id = '8ad60n';
+$campaign_id = 'h8ai43';
 
 
 $ta = new TALoader($campaign_id);
